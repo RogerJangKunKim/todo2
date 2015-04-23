81a0e2bf3151b36ad622e790bb6f10995cc1c61c
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-23 10:53:54 -0700

    Milestone 5.1

--- includes/connect.php
+++ includes/connect.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
 if (mysqli->connect_erro) { //if there's an error with the connection, we want to kill it and display Connect Error.
 	die('Connect Error (' . $mysqli->connect_errno . ')'
 		. mysqli->connect_error);
