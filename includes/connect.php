778420558902601d9a48b8a5989faa24f39bdbd7
Roger JangKun Kim <rb2stk@gmail.com>
2015-04-23 11:10:46 -0700

    Milestone 9 + fixed some errors

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,8 @@
 <?php
 $mysqli = new mysqli('localhost', 'root', 'root', 'todo2');
-if (mysqli->connect_erro) { //if there's an error with the connection, we want to kill it and display Connect Error.
+if ($mysqli->connect_error) { //if there's an error with the connection, we want to kill it and display Connect Error.
 	die('Connect Error (' . $mysqli->connect_errno . ')'
-		. mysqli->connect_error);
+		. $mysqli->connect_error);
 }
 else {	//if there is no connection erro, then echo:
 	echo "Connection made";
